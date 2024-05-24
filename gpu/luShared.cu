f31e50b23ccce0f4a81777fc179757056c11d7b9
sumukha <sumukha.shridhar@vectoflow.de>
2024-05-24 09:32:23 +0200

    shMem with A replaced by LU

--- gpu/luShared.cu
+++ gpu/luShared.cu
@@ -1,8 +1,8 @@
 #include "luShared.cuh"
 
 int main() {
-    FpType *A, *L, *U, *LU;
-    FpType *d_A, *d_L, *d_U;
+    FpType *A, *L, *U, *LU, *LandU;
+    FpType *d_A;
     int matrixSize, numThreads;
     // let user input matrixSize and numThreads
     printf("Enter matrix size: ");
@@ -17,12 +17,11 @@ int main() {
     L = (FpType *)malloc(numElements * sizeof(FpType));
     U = (FpType *)malloc(numElements * sizeof(FpType));
     LU = (FpType *)malloc(numElements * sizeof(FpType));
+    LandU = (FpType *)malloc(numElements * sizeof(FpType));
 
     cudaMalloc(&d_A, numElements * sizeof(FpType));
-    cudaMalloc(&d_L, numElements * sizeof(FpType));
-    cudaMalloc(&d_U, numElements * sizeof(FpType));
 
-    // FpType inputMatrix[] = {4, 11, 3, 4, 10, 4, 2, 4, 2};
+    FpType inputMatrix[] = {4, 11, 3, 4, 10, 4, 2, 4, 2};
 
     FILE *file = fopen("matrix.txt", "r");
 
@@ -32,24 +31,21 @@ int main() {
             fscanf(file, "%lf", &A[i * matrixSize + j]);
             // A[i * matrixSize + j] = inputMatrix[i * matrixSize + j];
             // A[i * matrixSize + j] = rand() % 10 + 1;
-            L[i * matrixSize + j] = 0.0;
-            U[i * matrixSize + j] = 0.0;
+            // L[i * matrixSize + j] = 0.0;
+            // U[i * matrixSize + j] = 0.0;
+            LandU[i * matrixSize + j] = 0.0;
         }
     }
 
     cudaMemcpy(d_A, A, numElements * sizeof(FpType), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_L, L, numElements * sizeof(FpType), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_U, U, numElements * sizeof(FpType), cudaMemcpyHostToDevice);
 
-    int shMemSize = 3 * matrixSize * matrixSize * sizeof(FpType);
+    int shMemSize = numElements * sizeof(FpType);
 
     startT = clock();
-    lu_decomp<<<1, numThreads, shMemSize>>>(d_A, d_L, d_U, matrixSize);
+    lu_decomp<<<1, numThreads, shMemSize>>>(d_A, matrixSize);
     endT = clock();
 
-    cudaMemcpy(A, d_A, numElements * sizeof(FpType), cudaMemcpyDeviceToHost);
-    cudaMemcpy(L, d_L, numElements * sizeof(FpType), cudaMemcpyDeviceToHost);
-    cudaMemcpy(U, d_U, numElements * sizeof(FpType), cudaMemcpyDeviceToHost);
+    cudaMemcpy(LandU, d_A, numElements * sizeof(FpType), cudaMemcpyDeviceToHost);
 
     printf("Time taken: %f\n", (endT - startT) / CLOCKS_PER_SEC);
 
@@ -57,10 +53,28 @@ int main() {
     // printf("Orig A:\n");
     // for (i = 0; i < matrixSize; i++) {
     //     for (j = 0; j < matrixSize; j++) {
-    //         printf("%f ", A[i * matrixSize + j]);
+    //         printf("%f ", LandU[i * matrixSize + j]);
     //     }
     //     printf("\n");
     // }
+
+    // assign L and U
+    for (i = 0; i < matrixSize; i++) {
+        for (j = 0; j < matrixSize; j++) {
+            if (i > j) {
+                L[i * matrixSize + j] = LandU[i * matrixSize + j];
+                U[i * matrixSize + j] = 0.0;
+            }
+            else if (i == j) {
+                L[i * matrixSize + j] = 1.0;
+                U[i * matrixSize + j] = LandU[i * matrixSize + j];
+            }
+            else {
+                L[i * matrixSize + j] = 0.0;
+                U[i * matrixSize + j] = LandU[i * matrixSize + j];
+            }
+        }
+    }
 
     // // print U
     // printf("U:\n");
@@ -102,20 +116,20 @@ int main() {
 
     if (fabs(diff) < 10e-6) {
         printf("LU decomposition is correct\n");
-    } else {
+    }
+    if (fabs(diff) > 10e-6) {
         printf("LU decomposition is incorrect\n");
     }
 
     printf("Diff is %f\n", diff);
 
     cudaFree(d_A);
-    cudaFree(d_L);
-    cudaFree(d_U);
 
     free(A);
     free(L);
     free(U);
     free(LU);
+    free(LandU);
 
     return 0;
 }
