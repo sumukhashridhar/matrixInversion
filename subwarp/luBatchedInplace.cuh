4f0782b428736dd2e8c156178ffeec48b87c42c6
sumukha <sumukha.shridhar@vectoflow.de>
2024-07-10 03:30:32 +0200

    subwarp, template, cuBLAS

---
+++ subwarp/luBatchedInplace.cuh
@@ -0,0 +1,127 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include "verify.hpp"
+
+#define CUDA_CHECK(call) { cudaError_t err = call; if (err != cudaSuccess) { printf("CUDA error: %s, line %d\n", cudaGetErrorString(err), __LINE__); exit(1); } }
+
+template<typename T>
+__device__ void comp_U(T* A, int rowIdx, int threadNum, int matrixSize) {
+    if (threadNum >= rowIdx && threadNum < matrixSize) {
+        T sum = static_cast<T>(0.0);
+
+        #pragma unroll
+        for (int l = 0; l < rowIdx; l++) {
+            sum += A[rowIdx * matrixSize + l] * A[l * matrixSize + threadNum]; 
+        }
+        A[rowIdx * matrixSize + threadNum] = A[rowIdx * matrixSize + threadNum] - sum;
+        // printf("A[%d][%d]: %f\n", rowIdx, threadNum, A[rowIdx * matrixSize + threadNum]);
+    }
+
+    else {
+        return;
+    }
+}
+
+
+template<typename T>
+__device__ void comp_L(T* A, int rowIdx, int threadNum, int matrixSize) {
+    if (threadNum > rowIdx && threadNum < matrixSize) {
+        T sum = static_cast<T>(0.0);
+
+        #pragma unroll
+        for (int l = 0; l < rowIdx; l++) {
+            sum += A[threadNum * matrixSize + l] * A[l * matrixSize + rowIdx];
+        }
+        A[threadNum * matrixSize + rowIdx] = (A[threadNum * matrixSize + rowIdx] - sum) / A[rowIdx * matrixSize + rowIdx];
+        // printf("A[%d][%d]: %f\n", threadNum, rowIdx, A[threadNum * matrixSize + rowIdx]);
+    }
+
+    else {
+        return;
+    }
+}
+
+
+template<typename T>
+__device__ void inversion(T* A, int rowIdx, int matrixSize) {
+    if (rowIdx < matrixSize) {
+        T y[128];
+        // init e on registers
+        // #pragma unroll
+        // for (int i = 0; i < matrixSize; i++) {
+        //     y[i] = 0.0;
+        // }
+
+        // forward substitution
+        for (int i = 0; i < matrixSize; i++) {
+            T sum = static_cast<T>(0.0);
+            #pragma unroll
+            for (int j = 0; j < i; j++) {
+                sum += A[(i * matrixSize) + j] * y[j];
+            }
+            y[i] = (((i == rowIdx) ? static_cast<T>(1.0) : static_cast<T>(0.0)) - sum);
+        }
+
+        // backward substitution
+        for (int i = matrixSize - 1; i >= 0; i--) {
+            T sum = static_cast<T>(0.0);
+            #pragma unroll
+            for (int j = i + 1; j < matrixSize; j++) {
+                sum += A[(i * matrixSize) + j] * A[(j * matrixSize) + rowIdx];
+            }
+            A[(i * matrixSize) + rowIdx] = (y[i] - sum) / A[(i * matrixSize) + i]; 
+        }
+    }
+
+    else {
+        return;
+    }
+}
+
+
+template<typename T>
+__global__ void batched_lu_subwarp(T* A, int matrixSize, int numMatrices, int threadsPerMatrix, int matricesPerBlock) {
+    int blockNum = blockIdx.x;
+    int threadIdInBlock = threadIdx.x;
+    int matrixIdInBlock = threadIdInBlock / threadsPerMatrix;
+    int globalMatrixId = blockNum * matricesPerBlock + matrixIdInBlock;
+
+    if (globalMatrixId < numMatrices) {
+        int threadIdInMatrix = threadIdInBlock % threadsPerMatrix;
+        int numElements = matrixSize * matrixSize;
+        int offset = globalMatrixId * numElements;
+        
+        extern __shared__ T shmem[];
+        T *sh_A = &shmem[matrixIdInBlock * numElements];
+
+        for (int k = threadIdInMatrix; k < numElements; k += threadsPerMatrix) {
+            sh_A[k] = A[k + offset];
+        }
+        __syncthreads();
+
+        for (int rowIdx = 0; rowIdx < matrixSize; rowIdx++) {
+            for (int threadNum = threadIdInMatrix; threadNum < matrixSize; threadNum += threadsPerMatrix) {
+                comp_U(sh_A, rowIdx, threadNum, matrixSize);
+            }
+            __syncthreads();
+            
+            for (int threadNum = threadIdInMatrix; threadNum < matrixSize; threadNum += threadsPerMatrix) {
+                comp_L(sh_A, rowIdx, threadNum, matrixSize);
+            }
+            __syncthreads();
+        }
+
+        for (int rowIdx = threadIdInMatrix; rowIdx < matrixSize; rowIdx += threadsPerMatrix) {
+            inversion(sh_A, rowIdx, matrixSize);
+        }
+        __syncthreads();
+
+        for (int k = threadIdInMatrix; k < numElements; k += threadsPerMatrix) {
+            A[k + offset] = sh_A[k];
+        }
+    }
+
+    __syncwarp();
+}
